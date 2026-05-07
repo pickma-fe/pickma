33fe76bf848b229bd8e9f634a8d9996dac4a604c
강이슬 <dew@gang-iseul-ui-MacBookPro.local>
2026-05-07 11:16:21 +0900

    fix: Modal export 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,6 +4,7 @@ export { Dropdown } from './Dropdown/Dropdown';
 export { Footer } from './Footer/Footer';
 export { Header } from './Header/Header';
 export { Input } from './Input/Input';
+export { Modal } from './Modal/Modal';
 export { Section } from './Section/Section';
 export { Sidebar } from './Sidebar/Sidebar';
 
