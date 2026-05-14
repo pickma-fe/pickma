825b59f2b085c1d9eccd831da1a67c952f2524a0
DrCloy <alsry0712@gmail.com>
2026-05-14 15:52:27 +0900

    chore: storage bucket 설정을 초기 스키마 파일로 통합

--- supabase/migrations/20260514000000_storage_buckets.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO storage.buckets (id, name, public) VALUES
-  ('seller-application-documents', 'seller-application-documents', false),
-  ('store-images', 'store-images', true),
-  ('product-images', 'product-images', true),
-  ('profile-images', 'profile-images', true)
-ON CONFLICT (id) DO NOTHING;
