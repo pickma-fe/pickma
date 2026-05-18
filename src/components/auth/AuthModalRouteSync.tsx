f7d5e18b00e2aef1ced362bc68b5606f08e69c8f
DrCloy <alsry0712@gmail.com>
2026-05-18 17:45:22 +0900

    feat: AuthModal을 auth hook 소비로 교체하고 AuthModalRouteSync 추가

---
+++ src/components/auth/AuthModalRouteSync.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { useAuthModal } from './useAuthModal';
+
+export function AuthModalRouteSync() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { openAuthModal } = useAuthModal();
+
+  useEffect(() => {
+    const auth = searchParams.get('auth');
+    const next = searchParams.get('next') ?? undefined;
+
+    if (auth !== 'required') return;
+
+    openAuthModal('login', next);
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete('auth');
+    params.delete('next');
+    const qs = params.toString();
+    router.replace(qs ? `?${qs}` : window.location.pathname, { scroll: false });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return null;
+}
