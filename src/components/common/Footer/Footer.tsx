b7ab68b36783e3174ae0d9f49d18ff2d0bed201c
DrCloy <alsry0712@gmail.com>
2026-06-16 14:51:04 +0900

    fix: Footer 상단 여백 축소

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { SupportSection } from './SupportSection';
 
 export function Footer() {
   return (
-    <footer className="mt-20 border-t border-gray-200 bg-gray-50">
+    <footer className="mt-12 border-t border-gray-200 bg-gray-50">
       <div className="mx-auto max-w-7xl px-14 pt-10 pb-20">
         <div className="grid grid-cols-1 lg:grid-cols-12">
           <div className="border-b border-gray-200 pb-6 lg:col-span-3 lg:border-r lg:border-b-0 lg:pr-6 lg:pb-0">
